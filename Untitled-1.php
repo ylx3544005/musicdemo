13638fb512b1a66202af714d29eef592ce489fa2
ylx <ylx3544005@126.com>
2014-11-04 20:03:43 +0800

    init commit

---
+++ Untitled-1.php
@@ -0,0 +1,331 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<script src="js/jquery-2.1.1.min.js"></script>
+<meta http-equiv="Access-Control-Allow-Origin" content="*">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="css/stylesheets/style.css" media="all">
+<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,initial-scale=1.0,user-scalable=no">
+<title>QQ音乐</title>
+<meta name="format-detection" content="telephone=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<style>
+@media only screen and (min-width : 320px) and (max-width : 480px) {	
+.mobile-tuwen-tu img{
+	width:100%;
+	height:auto;
+	margin:auto;
+}
+
+}
+.mobile-tuwen-h2{
+	color:#fff;
+	font-size:18px;
+	font-family:Georgia, "Times New Roman", Times, serif;
+	background-color:#4481C4;
+	width:100%;
+	height:40px;
+	line-height:40px;
+	padding-left:20px;
+}
+.mobile-tuwen-bf{
+    display:inline-block;
+	height:40px;
+	float:left;
+	width:40px;
+	margin-left:12px;
+	-moz-border-radius: 20px;
+    -webkit-border-radius: 20px;
+    -o-border-radius: 20px;
+    -ms-border-radius: 20px;
+    -khtml-border-radius: 20px;
+    border-radius: 20px;
+}
+.mobile-tuwen-wz{
+	display:inline-block;
+	width:auto;
+	height:40px;
+	float:left;
+	line-height:40px;
+}
+.mobile-tuwen-tu{
+	width:100%;
+	height:auto;
+	margin:auto;
+}
+.mobile-tuwen-wen{
+	width:100%;
+	height:auto;
+	margin:0;
+	font-size:13px;
+	font-family:Georgia, "Times New Roman", Times, serif;
+	color:#4481C4;
+	text-indent:28px;
+	text-align:left;
+	text-justify:auto;
+	text-align:justify;
+}
+.mobile-tuwen-wen-p{
+	padding:0px 10px;
+}
+.mobile-tuwen-ul{
+	padding-left:20px;
+	padding-top:10px;
+}
+.mobile-tuwen-ul li{
+	margin-top:10px;
+	height:40px;
+	border-bottom:1px dashed #4481C4;
+
+}
+.mobile-tuwen-ul-num{
+	color:#4481C4;
+	font-size:20px;
+	padding-right:10px;
+	height:40px;
+	line-height:40px;
+	display:inline-block;
+	text-align:center;
+	width:20px;
+	float:left;
+
+}
+.mobile-tuwen-ul-cont{
+	display:inline-block;
+	height:40px;
+	width:auto;
+
+}
+.mobile-tuwen-ul-tit{
+	display:inline-block;
+	width:auto;
+	height:20px;
+	color:#1C7B47;
+	font-weight:bold;
+}
+.mobile-tuwen-ul-art{
+	display:block;]
+	width:auto;
+	height:20px;
+	color:#4481C4;
+}
+.mobile-tuwen-ul-more{
+	text-align:center;
+	width:100%;
+	height:20px;
+	font-size:13px;	
+}
+.mobile-tuwen-ul-more a{
+	color:#4481C4;
+}
+</style>
+
+</head>
+<body>
+
+    <div>
+    <div class="mobile-tuwen-h2">
+        <span class="mobile-tuwen-wz">香格里拉</span>
+        <span class="hidecont" style="display:none; visibility:hidden; width:0; height:0;">
+                {title: '肩上蝶',
+                artist: '金志文',
+                album: '肩上蝶',
+                cover:'http://attach.hunantv.com/uploads/images/cms_img/201212/02/18/10504925422628832430.jpg',
+                mp3: 'http://112.124.44.10:8080/song1/api/aliyun/v1/geturl?key=Cherry Time/场景/懒在沙发上/古典/优美钢琴小品集/舒伯特 - 小夜曲.mp3'
+                }
+       </span>
+       <img src="css/images/u=3313055982,3668831902&fm=21&gp=0.jpg" width="40px" height="40px" class="mobile-tuwen-bf"/>
+    </div>   
+    <div class="mobile-tuwen-tu">
+        <img src="css/images/12870358665758.jpg"/>
+    </div>
+    <div class="mobile-tuwen-wen">
+        <p class="mobile-tuwen-wen-p">专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字 </p>
+    </div>
+    </div> 
+  
+    <div>
+    <div class="mobile-tuwen-h2">
+        <span class="mobile-tuwen-wz">香格里拉</span>
+                <span class="hidecont" style="display:none; visibility:hidden; width:0; height:0;">
+                {title: '肩上蝶',
+                artist: '金志文',
+                album: '肩上蝶',
+                cover:'http://attach.hunantv.com/uploads/images/cms_img/201212/02/18/10504925422628832430.jpg',
+                mp3: 'http://112.124.44.10:8080/song1/api/aliyun/v1/geturl?key=Cherry Time/场景/懒在沙发上/古典/优美钢琴小品集/舒伯特 - 小夜曲.mp3'
+                }
+       </span>
+        <img src="css/images/u=3313055982,3668831902&fm=21&gp=0.jpg" width="40px" height="40px" class="mobile-tuwen-bf"/>
+    </div>   
+    <div class="mobile-tuwen-tu">
+        <img src="css/images/70ad780ab5c0e88be4d10e2f8629715f.jpg"/>
+    </div>
+    <div class="mobile-tuwen-wen">
+        <p class="mobile-tuwen-wen-p">专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字 </p>
+    </div>
+    </div> 
+  
+    <div>
+    <div class="mobile-tuwen-h2">
+        <span class="mobile-tuwen-wz">香格里拉</span>
+        <span class="hidecont" style="display:none; visibility:hidden; width:0; height:0;">
+                {title: '肩上蝶',
+                artist: '金志文',
+                album: '肩上蝶',
+                cover:'http://attach.hunantv.com/uploads/images/cms_img/201212/02/18/10504925422628832430.jpg',
+                mp3: 'http://112.124.44.10:8080/song1/api/aliyun/v1/geturl?key=Cherry Time/场景/懒在沙发上/古典/优美钢琴小品集/舒伯特 - 小夜曲.mp3'
+                }
+       </span>
+        
+        <img src="css/images/u=3313055982,3668831902&fm=21&gp=0.jpg" width="40px" height="40px" class="mobile-tuwen-bf"/>
+    </div>   
+    <div class="mobile-tuwen-tu">
+        <img src="css/images/20100502013256-1385711783.jpg"/>
+    </div>
+    <div class="mobile-tuwen-wen">
+        <p class="mobile-tuwen-wen-p">专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字专辑描叙文字 </p>
+    </div>
+    </div> 
+  
+    <div>
+      <div class="mobile-tuwen-h2">
+        <span class="mobile-tuwen-wz">原创音乐排行版</span>
+       <span class="hidecont" style="display:none; visibility:hidden; width:0; height:0;">
+                {title: '肩上蝶',
+                artist: '金志文',
+                album: '肩上蝶',
+                realImgURL:'http://attach.hunantv.com/uploads/images/cms_img/201212/02/18/10504925422628832430.jpg',
+                realFileURL: 'http://112.124.44.10:8080/song1/api/aliyun/v1/geturl?key=Cherry Time/场景/懒在沙发上/古典/优美钢琴小品集/舒伯特 - 小夜曲.mp3'
+                }
+       </span>
+       <img src="css/images/u=3313055982,3668831902&fm=21&gp=0.jpg" width="40px" height="40px" class="mobile-tuwen-bf"/>
+      </div> 
+    <ul class="mobile-tuwen-ul">
+    <li><span class="mobile-tuwen-ul-num">1</span><span class="mobile-tuwen-ul-cont"><a href="#" class="mobile-tuwen-ul-tit">香格里拉</a><a href="#"><span class="mobile-tuwen-ul-art">作者</span></a></span></li>
+    <li><span class="mobile-tuwen-ul-num">2</span><span class="mobile-tuwen-ul-cont"><a href="#" class="mobile-tuwen-ul-tit">去大理</a><a href="#"><span class="mobile-tuwen-ul-art">作者</span></a></span></li>
+    <li><span class="mobile-tuwen-ul-num">3</span><span class="mobile-tuwen-ul-cont"><a href="#" class="mobile-tuwen-ul-tit">大约在冬季</a><a href="#"><span class="mobile-tuwen-ul-art">作者</span></a></span></li>
+    <li><span class="mobile-tuwen-ul-num">4</span><span class="mobile-tuwen-ul-cont"><a href="#" class="mobile-tuwen-ul-tit">爱</a><a href="#"><span class="mobile-tuwen-ul-art">作者</span></a></span></li>
+    <li><span class="mobile-tuwen-ul-num">5</span><span class="mobile-tuwen-ul-cont"><a href="#" class="mobile-tuwen-ul-tit">爱我别走</a><a href="#"><span class="mobile-tuwen-ul-art">作者</span></a></span></li>
+    </ul>
+    <p class="mobile-tuwen-ul-more"><a href="#">收听更多榜单内容</a></p>
+    </div>
+	<!-- 内容区 -->
+	<div id="mod_main">
+		<div class="crtPage mod_slide">
+         <div class="songlist">
+           <div class="mod_song_list ui-refresh newlist">
+           <ul id="playlist">
+                
+          <?php
+          
+		   $inum=1;
+		   
+		   function datalist($i=1){
+			   
+		   $realURL="http://112.124.44.10:8080/song1/api/cherrytime/v1/songs?songlistid=".$i ;
+		   
+           $handle=fopen($realURL, "r");
+		   
+		   $content="";
+		  
+		   
+		   while(!feof($handle)){
+			   
+			   
+			 $content.=fread($handle, 1000) ;   
+			 
+			   
+		  }
+           
+
+		    return $content;
+			
+		   fclose($handle);
+		   
+		   
+		   }
+		   
+		   
+           ?>
+           
+          <script type="text/javascript">
+     
+          var datajs=function(num){
+				
+		  var num=parseInt(num);
+					
+		  var datalistt=<?=datalist()?>;
+		 
+		  return datalistt["data"]; 
+		  
+		   
+		  }
+		  
+		  
+		  function datajsmore(){
+		
+			  	  
+		 return datalistt["data"];
+			  
+			  
+			  
+		  }
+		  
+		  
+		   
+
+           </script>
+          
+   
+           
+           
+           </ul>  
+    <div class="ui-refresh-down on"><span class="ui-refresh-icon"></span><span class="ui-refresh-label" 
+    onClick="javascript:datajsmore()">加载更多</span></div></div></div></div>
+	</div>
+    <div id="player">
+		<div class="cover"></div>
+		<div class="yctrl">
+			<div class="tag">
+				<strong>Title</strong>
+				<span class="artist">Artist</span>
+				<span class="album">Album</span>
+			</div>
+			<div class="control">
+				<div class="left">
+					<div class="rewind icon"></div>
+					<div class="playback icon"></div>
+					<div class="fastforward icon"></div>
+				</div>
+				<div style="dislpay:none; visibility:hidden; height:0px; width:0px;" class="volume right">
+					<div class="mute icon left"></div>
+					<div class="slider left">
+						<div class="pace"></div>
+					</div>
+				</div>
+			</div>
+			<div class="progress">
+				<div class="slider">
+					<div class="loaded"></div>
+					<div class="pace"></div>
+				</div>
+				<div style="dislpay:none; visibility:hidden; height:0px; width:0px;" class="timer left">0:00</div>
+				<div class="right">
+					<div class="repeat icon"></div>
+					<div class="shuffle icon"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--<audio preload="auto"></audio>-->
+	<div id="tip">
+	  <span></span>
+	</div>
+    <script src="js/jquery-ui-1.10.4.custom.min.js"></script>
+	<script src="js/script.js"></script>
+    <div class="gmu-media-detect" id="gmu-media-detect0"></div>
+    
+         
+
+    </body>
+    </html>
---
+++ js/script.js
@@ -0,0 +1,244 @@
+﻿function qcaudio(listdata){
+	// Settings
+	var repeat = localStorage.repeat || 0, //循环参数
+		shuffle = localStorage.shuffle || 'false', // 随机参数
+		continous = true,  
+		autoplay = true,
+		playlist =listdata;
+		
+	// Load playlist <li><h6 class="main_tit">安静听完这一首</h6><p class="sub_tit">覃桢</p></li>
+	for (var i=0; i<playlist.length; i++){
+		var item = playlist[i];
+		$('#playlist').append('<li><h6 class=main_tit>'+item.singer+'</h6><p class=sub_tit>'+item.singer+'</p></li>');
+	}
+
+	var time = new Date(),
+		currentTrack = shuffle === 'true' ? time.getTime() % playlist.length : 0, //随机歌曲当前的参数
+		trigger = false,
+		audio, timeout, isPlaying, playCounts;  //参数设置
+
+	var play = function(){
+		audio.play();
+		$('.playback').addClass('playing');
+		timeout = setInterval(updateProgress, 500);
+		isPlaying = true;
+	}
+
+	var pause = function(){
+		audio.pause();
+		$('.playback').removeClass('playing');
+		clearInterval(updateProgress);
+		isPlaying = false;
+	}
+
+	// Update progress  进度条函数
+	var setProgress = function(value){
+		var currentSec = parseInt(value%60) < 10 ? '0' + parseInt(value%60) : parseInt(value%60),
+			ratio = value / audio.duration * 100;
+
+		$('.timer').html(parseInt(value/60)+':'+currentSec);
+		$('.progress .pace').css("width", ratio + "%");
+		$('.progress .slider a').css("left", ratio + "%");
+	}
+
+	var updateProgress = function(){
+		setProgress(audio.currentTime);
+	}
+
+	// Progress slider //进度条滑动与暂停
+	$('.progress .slider').slider({step: 0.1, slide: function(event, ui){
+		$(this).addClass('enable');
+		setProgress(audio.duration * ui.value / 100);
+		clearInterval(timeout);
+	}, stop: function(event, ui){
+		audio.currentTime = audio.duration * ui.value/ 100;
+		$(this).removeClass('enable');
+		timeout = setInterval(updateProgress, 500);
+	}});
+
+	// Volume slider
+	var setVolume = function(value){
+		audio.volume = localStorage.volume = value;
+		$('.volume .pace').css('width', value * 100 + '%');
+		$('.volume .slider a').css('left', value * 100 + '%');
+	}
+
+	var volume = localStorage.volume || 0.5;
+	$('.volume .slider').slider({max: 1, min: 0, step: 0.01, value: volume, slide: function(event, ui){
+		setVolume(ui.value);
+		$(this).addClass('enable');
+		$('.mute').removeClass('enable');
+	}, stop: function(){
+		$(this).removeClass('enable');
+	}}).children('.pace').css('width', volume * 100 + '%');
+
+	$('.mute').click(function(){
+		if ($(this).hasClass('enable')){
+			setVolume($(this).data('volume'));
+			$(this).removeClass('enable');
+		} else {
+			$(this).data('volume', audio.volume).addClass('enable');
+			setVolume(0);
+		}
+	});
+
+	// Switch track
+	var switchTrack = function(i){
+		if (i < 0){
+			track = currentTrack = playlist.length - 1;
+		} else if (i >= playlist.length){
+			track = currentTrack = 0;
+		} else {
+			track = i;
+		}
+
+		$('audio').remove();
+		loadMusic(track);
+		if (isPlaying == true) play();
+	}
+
+	// Shuffle
+	var shufflePlay = function(){
+		var time = new Date(),
+			lastTrack = currentTrack;
+		currentTrack = time.getTime() % playlist.length;
+		if (lastTrack == currentTrack) ++currentTrack;
+		switchTrack(currentTrack);
+	}
+
+	// Fire when track ended
+	var ended = function(){
+		pause();
+		audio.currentTime = 0;
+		playCounts++;
+		if (continous == true) isPlaying = true;
+		if (repeat == 1){
+			play();
+		} else {
+			if (shuffle === 'true'){
+				shufflePlay();
+			} else {
+				if (repeat == 2){
+					switchTrack(++currentTrack);
+				} else {
+					if (currentTrack < playlist.length) switchTrack(++currentTrack);
+				}
+			}
+		}
+	}
+
+	var beforeLoad = function(){
+		var endVal = this.seekable && this.seekable.length ? this.seekable.end(0) : 0;
+		$('.progress .loaded').css('width', (100 / (this.duration || 1) * endVal) +'%');
+	}
+
+	// Fire when track loaded completely
+	var afterLoad = function(){
+		if (autoplay == true) play();
+	}
+
+	// Load track
+	var loadMusic = function(i){
+		var item = playlist[i];
+		
+		newaudio = $('<audio>').html('<source src="'+item.realFileURL+'"><source src="'+item.ogg+'">').appendTo('#player');
+		$('.cover').html('<img src="'+item.realImgURL+'" alt="'+item.album+'">');
+		$('.tag').html('<strong>'+item.title+'</strong><span class="artist">'+item.artist+'</span><span class="album">'+item.album+'</span>');
+		$('#playlist li').removeClass('playing').eq(i).addClass('playing');
+		audio = newaudio[0];
+		audio.volume = $('.mute').hasClass('enable') ? 0 : volume;
+		audio.addEventListener('progress', beforeLoad, false);
+		audio.addEventListener('durationchange', beforeLoad, false);
+		audio.addEventListener('canplay', afterLoad, false);
+		audio.addEventListener('ended', ended, false);
+	}
+
+	loadMusic(currentTrack);
+	$('.playback').on('click', function(){
+		if ($(this).hasClass('playing')){
+			pause();
+		} else {
+			play();
+		}
+	});
+	$('.rewind').on('click', function(){
+		if (shuffle === 'true'){
+			shufflePlay();
+		} else {
+			switchTrack(--currentTrack);
+		}
+	});
+	$('.fastforward').on('click', function(){
+		if (shuffle === 'true'){
+			shufflePlay();
+		} else {
+			switchTrack(++currentTrack);
+		}
+	});
+	$('#playlist li').each(function(i){
+		var _i = i;
+		$(this).on('click', function(){
+			switchTrack(_i);
+		});
+	});
+
+	if (shuffle === 'true') $('.shuffle').addClass('enable');
+	if (repeat == 1){
+		$('.repeat').addClass('once');
+	} else if (repeat == 2){
+		$('.repeat').addClass('all');
+	}
+
+
+//   点击播放音乐事件的功能
+     
+	$('.mobile-tuwen-bf').each(function(){
+		
+	   var self=this;
+	   
+       this.onclick=function(){
+		 
+	   $('audio').remove(); 
+		 //eval('(' + $(self).siblings('.hidecont').html() + ')')
+	   var jsoncont=eval('(' + $(self).siblings('.hidecont').html() + ')');
+
+	   //if (isPlaying == true) play();
+	   
+		newaudio = $('<audio>').html('<source src="'+jsoncont.realFileURL+'"><source src="'+jsoncont.ogg+'">').appendTo('#player');
+		$('.cover').html('<img src="'+jsoncont.realImgURL+'" alt="'+jsoncont.album+'">');
+		$('.tag').html('<strong>'+jsoncont.title+'</strong><span class="artist">'+jsoncont.artist+'</span><span class="album">'+jsoncont.album+'</span>');
+		audio = newaudio[0];
+		audio.volume = $('.mute').hasClass('enable') ? 0 : volume;
+		audio.addEventListener('progress', beforeLoad, false);
+		audio.addEventListener('durationchange', beforeLoad, false);
+		audio.addEventListener('canplay', afterLoad, false);
+		audio.addEventListener('ended', ended, false);  
+    }
+ 
+});
+
+	$('.repeat').on('click', function(){
+		if ($(this).hasClass('once')){
+			repeat = localStorage.repeat = 2;
+			$(this).removeClass('once').addClass('all');
+		} else if ($(this).hasClass('all')){
+			repeat = localStorage.repeat = 0;
+			$(this).removeClass('all');
+		} else {
+			repeat = localStorage.repeat = 1;
+			$(this).addClass('once');
+		}
+	});
+
+	$('.shuffle').on('click', function(){
+		if ($(this).hasClass('enable')){
+			shuffle = localStorage.shuffle = 'false';
+			$(this).removeClass('enable');
+		} else {
+			shuffle = localStorage.shuffle = 'true';
+			$(this).addClass('enable');
+		}
+	});
+};
+
+qcaudio(datajsmore());
